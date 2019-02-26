34a1c9fe1602aab37995d1d50fc7bc6aec8bd495
Anton_Tsymbal <Anton_Tsymbal@epam.com>
2019-02-26 13:38:03 +0200

    Update prototype for targets detection and visualisation

--- app/chessboard.py
+++ app/chessboard.py
@@ -29,27 +29,30 @@ class Chessboard(object):
             [Rook(Color.WHITE), Pawn(Color.WHITE), Empty(), Empty(), Empty(), Empty(), Pawn(Color.BLACK), Rook(Color.BLACK)],
             [Knight(Color.WHITE), Pawn(Color.WHITE), Empty(), Empty(), Empty(), Empty(), Pawn(Color.BLACK), Knight(Color.BLACK)],
             [Bishop(Color.WHITE), Pawn(Color.WHITE), Empty(), Empty(), Empty(), Empty(), Pawn(Color.BLACK), Bishop(Color.BLACK)],
-            [Queen(Color.WHITE), Pawn(Color.WHITE), Empty(), Empty(), Empty(), Empty(), Pawn(Color.BLACK), Queen(Color.BLACK)],
-            [King(Color.WHITE), Pawn(Color.WHITE), Empty(), Empty(), Empty(), Empty(), Pawn(Color.BLACK), King(Color.BLACK)],
+            [Queen(Color.WHITE), Pawn(Color.WHITE), Empty(), Bishop(Color.WHITE), Empty(), Empty(), Pawn(Color.BLACK), Queen(Color.BLACK)],
+            [King(Color.WHITE), Pawn(Color.WHITE), Empty(), Empty(), Bishop(Color.BLACK), Empty(), Pawn(Color.BLACK), King(Color.BLACK)],
             [Bishop(Color.WHITE), Pawn(Color.WHITE), Empty(), Empty(), Empty(), Empty(), Pawn(Color.BLACK), Bishop(Color.BLACK)],
             [Knight(Color.WHITE), Pawn(Color.WHITE), Empty(), Empty(), Empty(), Empty(), Pawn(Color.BLACK), Knight(Color.BLACK)],
             [Rook(Color.WHITE), Pawn(Color.WHITE), Empty(), Empty(), Empty(), Empty(), Pawn(Color.BLACK), Rook(Color.BLACK)]
         ]
 
     def get_targets(self, position):
-        x_position = self.x_position_to_index[position[0]]  # x position is from up to down
-        y_position = int(position[1]) - 1  # y position is from left to right
+        x_position = self.x_position_to_index[position[0]]  # x position is from left to right
+        y_position = int(position[1]) - 1  # y position is from down to up
 
         piece = self.board[x_position][y_position]
         moves = piece.get_moves()
 
         result = []
-        for move in moves:
-            x_target = x_position + move[0]
-            y_target = y_position + move[1]
-            if 0 <= x_target <= 7 and 0 <= y_target <= 7:
-                target_piece = self.board[x_target][y_target]
-                if target_piece.color != piece.color:
-                    result.append(self.index_to_x_position[x_target] + str(y_target + 1))
+        for single_direction_moves in moves:
+            for single_direction_move in single_direction_moves:
+                x_target = x_position + single_direction_move[0]
+                y_target = y_position + single_direction_move[1]
+                if 0 <= x_target <= 7 and 0 <= y_target <= 7:
+                    target_piece = self.board[x_target][y_target]
+                    if target_piece.color != piece.color:
+                        result.append(self.index_to_x_position[x_target] + str(y_target + 1))
+                    if target_piece.color != Color.EMPTY:
+                        break
 
         return result
